8ea9fbaa044ce21e3b4b7c0e63e24eedbca637ad
OONULL <1224204958@qq.com>
2020-05-04 18:59:48 +0800

    测试项目文件

---
+++ src/main/java/com/sq/test.java
@@ -0,0 +1,12 @@
+package com.sq;
+
+/**
+ * @author sq
+ * @version 1.0
+ * @date 2020/5/4 18:57
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("2333333333333333333");
+    }
+}
