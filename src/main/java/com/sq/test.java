fea840892f67c41bb097def21265ab54b51f3e13
OONULL <1224204958@qq.com>
2020-05-04 19:04:39 +0800

    测试项目文件,第一次更新

--- src/main/java/com/sq/test.java
+++ src/main/java/com/sq/test.java
@@ -8,5 +8,6 @@ package com.sq;
 public class test {
     public static void main(String[] args) {
         System.out.println("2333333333333333333");
+        System.out.println("6666666");
     }
 }
